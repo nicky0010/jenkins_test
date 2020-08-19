6bcda61a6cd51d82f7b50f45741f6dbf29112905
nicky0010 <oto.ordeen@gmail.com>
2020-08-20 00:53:38 +0900

    modified helloworld

--- hello_world.groovy
+++ hello_world.groovy
@@ -3,7 +3,11 @@ pipeline {
     stages {
         stage('hello') {
             steps {
-                deleteDir()
+                script {
+					commonProperties = readYaml file: "${pwd()}@script/jenkinsfiles/nicky-artifact/envs/common.yml"
+					props = commonProperties.props
+					mavenSetting = "${pwd()}@script/${props.mavenSettingXmlPath}"
+				}
                 echo 'hello world'
             }
         }
