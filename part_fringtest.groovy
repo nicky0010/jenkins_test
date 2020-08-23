341db184c0c75325c7fa8e447eae64a27053585e
nicky0010 <oto.ordeen@gmail.com>
2020-08-24 01:29:47 +0900

    echo pass

--- part_fringtest.groovy
+++ part_fringtest.groovy
@@ -4,10 +4,11 @@ pipeline {
 		stage('Publish') {
 			steps {
 				script {
+					echo "${workspace}"
+					echo "${pwd()}"
 					commonProperties = readYaml file: "${pwd()}@script/jenkinsfiles/nicky-artifact/envs/common.yml"
 					props = commonProperties.props
 					mavenSetting = "${pwd()}@script/${props.mavenSettingXmlPath}"
-					echo "${workspace}"
 				}
 			}
 		}
