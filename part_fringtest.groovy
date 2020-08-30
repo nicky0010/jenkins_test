0091d9ddf6a72596b326286aaa8f839974c01dc6
nicky0010 <oto.ordeen@gmail.com>
2020-08-30 23:53:38 +0900

    add pra

--- part_fringtest.groovy
+++ part_fringtest.groovy
@@ -2,6 +2,27 @@ properties([
     parameters([
 		string(name: 'brEdfBranch', defaultValue: 'support/v1.0', description: 'github target branch'),
 		string(name: 'brEdfultilsBranch', defaultValue: 'support/v1.0', description: 'github target branch'),
+		[
+			$class      : 'ChoiceParameter',
+			choiceType  : 'PT_CHECKBOX',
+			description : 'target deploy component',
+			filterLength: 1,
+			filterable  : true,
+			name        : 'components',
+			script      : [
+				$class        : 'GroovyScript',
+				fallbackScript: [
+					classpath: [],
+					sandbox  : false,
+					script   : 'return ["ERROR"]'
+				],
+				script        : [
+					classpath: [],
+					sandbox  : false,
+					script   : 'return[\'fr-edf\',\'fr-edf-utils\']'
+				]
+			]
+		],
 	])
 ])
 
